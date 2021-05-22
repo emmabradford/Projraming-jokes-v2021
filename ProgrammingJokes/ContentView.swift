7cd482793d0be676579951d57941858473151ec8
Emma Bradford <ebradford@Emmas-MacBook-Air.local>
2021-05-22 12:11:13 -0500

    finished project

---
+++ ProgrammingJokes/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  ProgrammingJokes
+//
+//  Created by Emma Bradford on 2/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View
+{
+    @State private var showingAlert = false
+    @State private var jokes = [Joke]()
+    var body: some View
+    {
+        NavigationView
+        {
+            List(jokes)
+            {
+                joke in NavigationLink(
+                    destination: Text(joke.punchline)
+                        .padding(),
+                    label: {
+                        Text(joke.setup)
+                    }
+                )
+            }
+            .navigationTitle("programing Jokes")
+            }
+        .onAppear(perform:
+        {
+            getJokes()
+        })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"), message: Text("issue loading data"), dismissButton: .default(Text("OK")))
+        })
+    }
+    
+    func getJokes()
+    {
+       let apiKey = "?rapidapi-key=2ec08bd9e5msh4725dc22b76a14cp1c3cc2jsn264479af3f9b"
+        let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+                if let url = URL(string: query)
+                {
+                    if let data = try? Data(contentsOf: url)
+                    {
+                        let json = try! JSON(data: data)
+                        if json["success"] == true
+                        {
+                            let contents = json["body"].arrayValue
+                            for  item in contents
+                            {
+                                let setup = item["setup"].stringValue
+                                let punchline = item["punchline"].stringValue
+                                let joke = Joke(setup: setup, punchline: punchline)
+                                jokes.append(joke)
+                            }
+                            return
+                        }
+                    }
+                }
+        showingAlert = true
+    }
+}
+
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        ContentView()
+    }
+}
+
+struct Joke: Identifiable
+{
+    let id = UUID()
+    var setup: String
+    var punchline: String
+}
---
+++ ProgrammingJokes/ProgrammingJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammingJokesApp.swift
+//  ProgrammingJokes
+//
+//  Created by Emma Bradford on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct ProgrammingJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
